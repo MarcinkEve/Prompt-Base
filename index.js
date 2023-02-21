551dbbcc8bbfa81ab9d04a36249e5452b507d1a6
MarcinkEve <marcinke@gmail.com>
2023-02-21 22:16:45 +0200

    creating table for data rendering

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log("labas");
 import { addPromptButton } from "./modules/addPromptButton.js";
+import { promptTable } from "./modules/promptTable.js";
+promptTable();
---
+++ modules/promptTable.js
@@ -0,0 +1,24 @@
+export const promptTable = () => {
+  const container = document.getElementById("table-container");
+
+  const table = document.createElement("table");
+  table.className = "prompt-table";
+  table.id = "prompt-table";
+  container.appendChild(table);
+
+  const tableHead = document.createElement("thead");
+  table.appendChild(tableHead);
+
+  const tableHeadRow = document.createElement("tr");
+  tableHead.appendChild(tableHeadRow);
+
+  const tableHeadings = ["Prompt", "Actions"];
+  tableHeadings.map((el) => {
+    const tableHeading = document.createElement("th");
+    tableHeading.innerText = el;
+    tableHeadRow.appendChild(tableHeading);
+  });
+
+  const tableBody = document.createElement("tbody");
+  table.appendChild(tableBody);
+};
